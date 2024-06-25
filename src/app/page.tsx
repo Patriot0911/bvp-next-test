acf4be5dce1e25c10ceae87af493b9a454720689
#Patriot0911 <75264092+Patriot0911@users.noreply.github.com>
2024-06-25 23:52:13 +0300

    env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,10 @@
 
 import { Dashboard } from '@/components';
 import { ThemeContextProvider } from '@/components';
-import { ThemeContext } from '@/libs/context';
-import { useContext } from 'react';
 
 const Home = () => {
-    const {
-        isDarkTheme,
-    } = useContext(ThemeContext);
-
     return (
-        <main
-            className={
-                isDarkTheme ? 'dark' : ''
-            }
-        >
+        <main>
             <ThemeContextProvider>
                 <Dashboard />
             </ThemeContextProvider>
